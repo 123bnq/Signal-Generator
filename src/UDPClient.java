6634980f0eb80e474b17f4f51cef3ed273ca7697
123bnq <33340665+123bnq@users.noreply.github.com>
2018-05-31 23:48:43 +0200

    new UDP server

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -4,27 +4,29 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.nio.charset.Charset;
 
 public class UDPClient {
 	private static DatagramSocket socket;
-	private static int port = 8888;
+	private static int port = 5555;
 	public static void main(String[] args) {
 		byte[] receiveData = new byte[1024];
         byte[] sendData = new byte[1024];
 		try {
-			InetAddress addr = InetAddress.getByName("192.168.137.1");
-			socket = new DatagramSocket(port, addr);
+			InetAddress addr = InetAddress.getByName("localhost");
+			socket = new DatagramSocket();
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length, addr, port);
+//			socket.receive(receivePacket);
 			String s = "Hello World";
 			sendData = s.getBytes();
-			System.out.println(sendData.toString());
+//			String st = new String(sendData);
+//			System.out.println(st);
 			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, addr, port);
 			socket.send(sendPacket);
+			socket.receive(receivePacket);
 			String sr;
-			do {
-				sr = new String(receivePacket.getData());
-				System.out.println(sr);
-			} while(!sr.equals("1"));
+			sr = new String(receivePacket.getData(), receivePacket.getOffset(), receivePacket.getLength());
+			System.out.println(sr);
 			System.out.println("Client is connected");
 			socket.close();
 		} catch (UnknownHostException e) {
---
+++ src/UDPServer.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPServer {
+	private static DatagramSocket socket;
+	private static byte[] buffer = new byte[250];
+	
+	public static void main(String[] args) {
+		try {
+			socket = new DatagramSocket(5555);
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			socket.receive(packet);
+			String st = new String(packet.getData(), packet.getOffset(), packet.getLength());
+			System.out.println(st);
+			InetAddress address = packet.getAddress();
+			int port = packet.getPort();
+			buffer = "HelloWorld".getBytes();
+			packet = new DatagramPacket(buffer, buffer.length, address, port);
+			socket.send(packet);
+			socket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
