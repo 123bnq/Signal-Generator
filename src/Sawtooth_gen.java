b72f2e46d46414e89bcc4a298d8413e80d11f481
123bnq <33340665+123bnq@users.noreply.github.com>
2018-06-13 23:44:37 +0200

    reupdated sawtooth

--- src/Sawtooth_gen.java
+++ src/Sawtooth_gen.java
@@ -42,7 +42,7 @@ public class Sawtooth_gen extends JComponent {
 	    g3.setColor(Color.red);
             
             Polygon pol1 = new Polygon();
-	    drawSawtooth(pol1, w, h,5, p1, 8000, p);
+	    drawSawtooth(pol1, w, h,50, p1, 8000, p);
 	    
 	    g3.drawPolyline(pol1.xpoints, pol1.ypoints, pol1.npoints);
 	}
@@ -82,60 +82,6 @@ public class Sawtooth_gen extends JComponent {
                     p1.addPoint(w+x,h-(int)y);
                 }
 }}
-
-
-
-
-/*	public void paintComponent(Graphics g) {
-		// w is x, and h is y (as in x/y values in a graph)
-		int w = this.getWidth() / 2;
-		int h = this.getHeight() / 2;
-
-		Graphics2D g1 = (Graphics2D) g;
-		g1.setStroke(new BasicStroke(2));
-		g1.setColor(Color.black);
-		g1.drawLine(0, h, w * 2, h);
-		g1.drawLine(w, 0, w, h * 2);
-		g1.drawString("0", w - 7, h + 13);
-		g1.setStroke(new BasicStroke(1));
-		g1.setColor(Color.gray);
-		for (int i = 1; i <= 3; i += 2) {
-			g1.drawLine(0, i * h / 2, w * 2, i * h / 2);
-			g1.drawLine(i * w / 2, 0, i * w / 2, h * 2);
-		}
-		int p = 100;
-		int p1 = 100;
-		super.paintComponent(g);
-
-		Graphics2D g3 = (Graphics2D) g;
-		g3.setStroke(new BasicStroke(2));
-		g3.setColor(Color.red);
-
-		Polygon pol1 = new Polygon();
-		drawSawtooth(pol1, w, h, p1, 8000, p);
-
-		g3.drawPolyline(pol1.xpoints, pol1.ypoints, pol1.npoints);
-	}*/
-
-	// public void drawSawtooth(int h, int w, int freq, Graphics2D g3){
-	// freq=(freq+20)/20;
-	// wave = new GeneralPath();
-	// float approxCycles = freq*w/(150);
-	// float dx = (w-1)/(float)Math.round(2*approxCycles);
-	// float dy = h/4;
-	// float step = 2*dx;
-	// int steps = (int)(w*2/step);
-	// float x = w, y = h;
-	// wave.moveTo(x,y);
-	// for(int j = 1; j < 2*steps; j++)
-	// {
-	// wave.lineTo(x+dx,y-dy);
-	// x += dx;
-	// wave.lineTo(x,y-dy);
-	// wave.lineTo(x,y+dy);
-	// }
-	// g3.draw(wave);
-	// }
 //	public void drawSawtooth(Polygon p1, int w, int h, int freq, int samplerate, int amp) {
 		// double y;
 //		double xprev = 0.0;
